f16ca9903e9e6eceebd36354c05a0210cdbbfa1b
Laurence <laurence.halfpenny@halfpenny.net>
2022-12-03 12:31:23 -0800

    Add initial example openapi json

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use serde_json;
+use openapiv3::OpenAPI;
+
+fn main() {
+    let openapi_json = include_str!("../examples/xkcd.json");
+    let openapi: OpenAPI = serde_json::from_str(openapi_json)
+        .expect("Could not deserialise input");
+    println!("{:?}", openapi.paths);
+}
